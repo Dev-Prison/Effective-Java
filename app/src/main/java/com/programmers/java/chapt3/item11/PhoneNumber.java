03955d886f26a7866d15409f71f673a26aedeabb
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-04-11 02:57:14 +0900

    item11

---
+++ app/src/main/java/com/programmers/java/chapt3/item11/PhoneNumber.java
@@ -0,0 +1,29 @@
+package com.programmers.java.chapt3.item11;
+
+public final class PhoneNumber {
+    private final short areaCode, prefix, lineNum;
+
+    public PhoneNumber(int areaCode, int prefix, int lineNum) {
+        this.areaCode = rangeCheck(areaCode, 999, "지역코드");
+        this.prefix   = rangeCheck(prefix,   999, "프리픽스");
+        this.lineNum  = rangeCheck(lineNum, 9999, "가입자 번호");
+    }
+
+    private static short rangeCheck(int val, int max, String arg) {
+        if (val < 0 || val > max)
+            throw new IllegalArgumentException(arg + ": " + val);
+        return (short) val;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof PhoneNumber))
+            return false;
+        PhoneNumber pn = (PhoneNumber)o;
+        return pn.lineNum == lineNum && pn.prefix == prefix
+                && pn.areaCode == areaCode;
+    }
+
+    // 나머지 코드는 생략 - hashCode 메서드는 꼭 필요하다(아이템 11)!
+}
