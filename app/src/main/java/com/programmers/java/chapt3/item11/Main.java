b44a4d64c42f07a501e867ffde74a01bcc6dede7
Yonghoon Lee <dldydgns53@naver.com>
2022-07-21 23:04:09 +0900

    docs: Item51

--- app/src/main/java/com/programmers/java/chapt3/item11/Main.java
+++ app/src/main/java/com/programmers/java/chapt3/item11/Main.java
@@ -6,13 +6,13 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Map<PhoneNumber, String> m = new HashMap<>();
-        m.put(new PhoneNumber(707,867,5309), "제니");
+        m.put(new PhoneNumber(707,867,5309), "jenny");
         String s = m.get(new PhoneNumber(707, 867, 5309));
         System.out.println(s); // null 출력
 
         // 기존 hashCode
         PhoneNumber original = new PhoneNumber(707, 867, 5309);
-        m.put(original, "제니");
+        m.put(original, "jenny");
         String jenni = m.get(original);
         System.out.println(jenni); //제니 출력
 
---
+++ app/src/main/java/com/programmers/java/chapt8/item51/MethodSignature.java
@@ -0,0 +1,11 @@
+package com.programmers.java.chapt8.item51;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodSignature {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+    }
+}
